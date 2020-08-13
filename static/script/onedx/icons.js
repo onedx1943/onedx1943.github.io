5b76587a472a0e78dff83a9aaf86b656027977ab
onedx <onedxu@gmail.com>
2020-08-13 21:50:25 +0800

    取消屏蔽代码

--- static/script/onedx/icons.js
+++ static/script/onedx/icons.js
@@ -1,16 +1,16 @@
 $(function () {
 
     let load_icons = function () {
-        // // 获取所有的css文件并筛选出font awesome对应的文件
-        // let css_files = document.styleSheets;
-        // for (let css_file of css_files) {
-        //     if (css_file.href.includes('font-awesome.min.css')) {
-        //         console.log(css_file);
-        //         //let data = css_file.cssRules;
-        //         let data = css_file.rules;
-        //         console.log(data);
-        //     }
-        // }
+        // 获取所有的css文件并筛选出font awesome对应的文件
+        let css_files = document.styleSheets;
+        for (let css_file of css_files) {
+            if (css_file.href.includes('font-awesome.min.css')) {
+                console.log(css_file);
+                //let data = css_file.cssRules;
+                let data = css_file.rules;
+                console.log(data);
+            }
+        }
         console.log(123);
     };
 
