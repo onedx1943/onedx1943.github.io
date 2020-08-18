93526062504d11ee03e0e439d6cfee5afc523fc9
onedx <onedxu@gmail.com>
2020-08-19 01:47:40 +0800

    添加背景图片

--- components/Home.js
+++ components/Home.js
@@ -1,9 +1,8 @@
 export default {
     template: `
-        <div>
-            <div>这是首页</div>
-            <div><router-link to="/app">Go to App</router-link></div>
-            <div><router-link to="/blog">Go to Blog</router-link></div>
+        <div class="home-content">
+            <router-link to="/app" class="home-content-button">Go to App</router-link>
+            <router-link to="/blog" class="home-content-button">Go to Blog</router-link>
         </div>
     `
 }
